a399b0c39327578525a3141f1118516e9ba9d824
kokojihye <jihyevivianako@gmail.com>
2023-11-01 12:46:18 +0900

    Initial commit

---
+++ src/main/java/com/chatgptapi/test/ChatGptApiApplication.java
@@ -0,0 +1,13 @@
+package com.chatgptapi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGptApiApplication.class, args);
+    }
+
+}
